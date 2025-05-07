c0a941ba46116a1e6994013f5bfd7cb542f36b82
Mohammed Baba <mbfmz20@gmail.com>
2025-05-07 10:46:26 +0100

    Mettre en place la configuration du CORS

--- backend/server.js
+++ backend/server.js
@@ -12,5 +12,10 @@ app.use(cors());
 app.use(express.json()); 
 app.use('/api/auth', authRoutes);
 
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+  }));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀Server running on port ${PORT}`));

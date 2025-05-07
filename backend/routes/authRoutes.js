5304a2f301a04a812f1c73d9650eb594367aebb7
Mohammed Baba <mbfmz20@gmail.com>
2025-05-07 15:37:07 +0100

    import validator dans routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const authCtrl = require('../controllers/authController');
+const { registerValidator, loginValidator } = require('../validators/authValidator');
 
-router.post('/register', authCtrl.register);
-router.post('/login', authCtrl.login);
+
+router.post('/register',registerValidator, authCtrl.register);
+router.post('/login',loginValidator, authCtrl.login);
 
 module.exports = router;

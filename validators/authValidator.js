ba2f33c3715882fa7cb48b54c9d4073c9117d447
Mohammed Baba <mbfmz20@gmail.com>
2025-05-07 15:27:42 +0100

    Intégrer express-validator pour valider les données (register, login)

---
+++ validators/authValidator.js
@@ -0,0 +1,22 @@
+const { body, validationResult } = require("express-validator");
+
+exports.registerValidator = [
+    body('name').notEmpty().withMessage('Nom requis'),
+  body('email').isEmail().withMessage('Email invalide'),
+  body('password').isLength({ min: 6 }).withMessage('Mot de passe trop court'),
+  (req, res, next)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
+    next();
+  }
+]; 
+
+exports.loginValidator = [
+  body('email').isEmail().withMessage('Email invalide'),
+  body('password').isLength({ min: 6 }).withMessage('Mot de passe trop court'),
+  (req, res, next)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
+    next();
+  }
+]; 

724dea49e84162e117fee393d1467bdd52a542ae
Mohammed Baba <mbfmz20@gmail.com>
2025-05-09 10:24:57 +0100

    implementer le role

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,22 +1,34 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt')
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, unique: true, required: true },
-  password: { type: String, required: true }
-});
+  name: {
+    type: String,
+    required: [true, 'Le nom est requis']
+  },
+  email: {
+    type: String,
+    required: [true, 'L’email est requis'],
+    unique: true
+  },
+  password: {
+    type: String,
+    required: [true, 'Le mot de passe est requis'],
+    minlength: 6
+  },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user'
+  }
+}, { timestamps: true });
 
 userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
-  
-  try {
-    const hash = await bcrypt.hash(this.password, 10);
-    this.password = hash;
-    next();
-  } catch (err) {
-    next(err);
-  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
-module.exports = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
+export default User;
